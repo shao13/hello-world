65fd065ab3ac87ce66973717963ab6affd45aad2
Shao-Hung Chan <shaohung1113@gmail.com>
2015-10-12 23:17:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-/*smooth scroll*/
 $(document).ready(function() {
+	setTimeout(function(){$("#loading_page").fadeOut('slow');},6000);
 	$("a").click(function(){
 	  var scroll = $(this).attr("href");
 	  $("body").animate({scrollTop: $(scroll).offset().top}, 1500);
 	  return false;
 	});
 });
+
